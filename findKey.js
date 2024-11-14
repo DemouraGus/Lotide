462bf20e128abce4cac92d0fae4bffa9d1e2e708
DemouraGus <demoura.gus@gmail.com>
2024-11-14 15:46:55 -0800

    findKey added and tested

---
+++ findKey.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    } 
+  }  
+  return undefined
+};
+
+const restaurantStars = {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  };
+
+const priceOfFruits = {
+  apple: 0.60,
+  avocado: 2.5,
+  lemon: 0.90,
+  lime: 1.10
+};
+
+assertEqual(findKey(restaurantStars, x => x.stars === 2), "noma");
+assertEqual(findKey(priceOfFruits, x => x > 2), "avocado");

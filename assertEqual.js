d5b1188a36aa35c4571d10c0da790fbc1093d322
DemouraGus <demoura.gus@gmail.com>
2024-11-18 08:27:32 -0800

    function assertEqual and head refactored so they export their functions. Also created test subdirectory and moved tests from aforementioned functions to their respective files

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual(`Lighthouse Labs`, `Bootcamp`);
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual(`Gus`, `Goose`);
+module.exports = assertEqual;
